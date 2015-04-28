e57d955cae2eedab05a2803e76e175494a63e512
Brian Marks <brianpmarks@gmail.com>
2015-04-28 18:33:36 +0000

    Adds support for sqrt.

--- numpy/__init__.js
+++ numpy/__init__.js
@@ -801,6 +801,15 @@ var $builtinmodule = function (name) {
   exp_f.$defaults = [0, new Sk.builtin.list([])];
   mod.exp = new Sk.builtin.func(exp_f);
 
+  // Square Root
+  var sqrt_f = function (x, out) {
+    Sk.builtin.pyCheckArgs("sqrt", arguments, 1, 2);
+    return callTrigonometricFunc(x, np.math ? np.math.sqrt : Math.sqrt);
+  };
+  sqrt_f.co_varnames = ['x', 'out'];
+  sqrt_f.$defaults = [0, new Sk.builtin.list([])];
+  mod.sqrt = new Sk.builtin.func(sqrt_f);
+
   /* Simple reimplementation of the linspace function
    * http://docs.scipy.org/doc/numpy/reference/generated/numpy.linspace.html
    */
